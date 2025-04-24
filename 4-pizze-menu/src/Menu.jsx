1aa874de008cb195cbf8f7d0003d7123ea91c20d
EJohn <dev.ejohnpaguntalan@gmail.com>
2025-04-24 23:53:37 +0800

    use react fragments to return multiple elements with no wrapper

--- 4-pizze-menu/src/Menu.jsx
+++ 4-pizze-menu/src/Menu.jsx
@@ -11,12 +11,20 @@ function Menu() {
     <>
       <main className="menu">
         <h2>Our menu</h2>
+
         {numPizzas > 0 ? (
-          <ul className="pizzas">
-            {pizzas.map((pizza) => (
-              <Pizza key={pizza.name} pizzaObj={pizza} />
-            ))}
-          </ul>
+          // <React.Fragment key={"menu"}></React.Fragment> //* Use key on fragments only inside .map() or dynamic lists.
+          <>
+            <p>
+              Authentic Italian cuisine. 6 creative dishes to choose from. All
+              from our stone oven, all organic, all delicious.
+            </p>
+            <ul className="pizzas">
+              {pizzas.map((pizza) => (
+                <Pizza key={pizza.name} pizzaObj={pizza} />
+              ))}
+            </ul>
+          </>
         ) : (
           <p>No pizzas available</p>
         )}
@@ -28,4 +36,4 @@ function Menu() {
 export default Menu;
 
 // NOTE:
-// We dont use if else cause it doesnt produce any value
+// We dont use if else cause it doesnt produce any value in JSX. but we can inside the component function
