00b3892717153e042da2c486ff672139d7bdf401
EJohn <dev.ejohnpaguntalan@gmail.com>
2025-05-01 20:27:42 +0800

    add video

--- sample-projects/smartnotes/src/components/App/App.jsx
+++ sample-projects/smartnotes/src/components/App/App.jsx
@@ -36,9 +36,33 @@ function App() {
         </div>
       </Testimonial>
       <Features />
-      <Testimonial></Testimonial>
+      <Testimonial>
+        <h5 className="h5 testimonial__heading">
+          The real-time synchronization and AI-powered organization have made
+          our workflow more efficient than ever.
+        </h5>
+        <div className="testimonial__author-section">
+          <p className="text-reg testimonial__author">Sarah Johnson</p>
+          <p className="text-reg testimonial__organization">
+            TechSavvy Solutions
+          </p>
+        </div>
+        <img className="testimonial__quotes" src={doubleQoute} alt="quote" />
+      </Testimonial>
       <Video />
-      <Testimonial></Testimonial>
+      <Testimonial>
+        <h5 className="h5 testimonial__heading">
+          The real-time synchronization and AI-powered organization have made
+          our workflow more efficient than ever.
+        </h5>
+        <div className="testimonial__author-section">
+          <p className="text-reg testimonial__author">Sarah Johnson</p>
+          <p className="text-reg testimonial__organization">
+            TechSavvy Solutions
+          </p>
+        </div>
+        <img className="testimonial__quotes" src={doubleQoute} alt="quote" />
+      </Testimonial>
       <Pricing />
       <Testimonial></Testimonial>
       <FAQ>
--- sample-projects/smartnotes/src/components/Video/Video.jsx
+++ sample-projects/smartnotes/src/components/Video/Video.jsx
@@ -1,5 +1,32 @@
+import BRgradient from "../../assets/vid-bottom-right-gradient.png";
+import TLgradient from "../../assets/vid-top-left-gradient.png";
+import videoImg from "../../assets/video.png";
+
+import playButton from "../../assets/play-button.png";
+
 function Video() {
-  return <div></div>;
+  return (
+    <section className="video">
+      <div className="video__container">
+        <img className="video__image" src={videoImg} alt="video" />
+        <button
+          className="video__play"
+          style={{ backgroundImage: `url(${playButton})` }}
+        ></button>
+        <div className="video__overlay"></div>
+      </div>
+      <img
+        className="video-gradient video__tl-gradient"
+        src={TLgradient}
+        alt="gradient"
+      />
+      <img
+        className="video-gradient video__br-gradient"
+        src={BRgradient}
+        alt="gradient"
+      />
+    </section>
+  );
 }
 
 export default Video;
