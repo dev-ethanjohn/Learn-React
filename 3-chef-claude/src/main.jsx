aa258e4457aec566efec4b49a0deef83bff09dcb
EJohn <dev.ejohnpaguntalan@gmail.com>
2025-05-22 15:43:12 +0800

    Update .gitignore to ignore .env files

--- 3-chef-claude/src/main.jsx
+++ 3-chef-claude/src/main.jsx
@@ -1,11 +1,25 @@
 import { useState } from "react";
+import ClaudeRecipe from "./ClaudeRecipe";
+import IngredientsList from "./IngredientsList";
 
 export default function Main() {
   const [ingredients, setIngredients] = useState([]);
+  const [recipeShown, setRecipeShown] = useState(false);
 
-  const ingredientsListItems = ingredients.map((ingredient) => (
-    <li key={ingredient}>{ingredient}</li>
-  ));
+  /**
+   *
+   * 2. Move the list of ingredients <section> into its
+   *    own IngredientsList component.
+   *
+   * While you're considering how to structure things, consider
+   * where state is, think about if it makes sense or not to
+   * move it somewhere else, how you'll communicate between
+   * the parent/child components, etc.
+   *
+   * The app should function as it currently does when you're
+   * done, so there will likely be some extra work to be done
+   * beyond what I've listed above.
+   */
 
   function addIngredient(formData) {
     // event.preventDefault();
@@ -16,6 +30,10 @@ export default function Main() {
     console.log(newIngredient);
 
     setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+  }
+
+  function toggleRecipe() {
+    setRecipeShown((prev) => !prev);
   }
 
   return (
@@ -33,7 +51,15 @@ export default function Main() {
         />
         <button>Add ingredient</button>
       </form>
-      <ul>{ingredientsListItems}</ul>
+
+      {ingredients.length > 0 ? (
+        <IngredientsList
+          toggleRecipe={toggleRecipe}
+          ingredients={ingredients}
+        />
+      ) : null}
+
+      {recipeShown ? <ClaudeRecipe /> : null}
     </main>
   );
 }
